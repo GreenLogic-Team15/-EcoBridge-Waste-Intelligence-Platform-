8ea8a58d956804e1d1024fb966837174a93db0a5
Funmilayo Esther <funmilayoesther001@gmail.com>
2026-02-25 20:14:56 +0100

    installed gh-pages

--- ecobridge-frontend/src/pages/dashboards/PartnerHomepage.jsx
+++ ecobridge-frontend/src/pages/dashboards/PartnerHomepage.jsx
@@ -14,6 +14,7 @@ import {
   Grid3X3,
 } from "lucide-react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import Sidebar from "../components/layout/Sidebar";
 
 const PartnerHomepage = () => {
   const [activeTab, setActiveTab] = useState("dashboard");
